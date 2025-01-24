e6bfd64c3bbc3d0e2d6b1e49861b1c9e1ba3d9ef
Gawain <gawainli@outlook.com>
2025-01-24 20:26:12 +0800

    refactor:PLogger

--- Assets/ProxFramework/Runtime/_Framework/Logger/PLogger.cs
+++ Assets/ProxFramework/Runtime/_Framework/Logger/PLogger.cs
@@ -26,26 +26,23 @@ namespace ProxFramework
     {
         public static LogLevel filterLevel = LogLevel.Info;
         public static OutputType outputType = OutputType.Editor;
-        
+
         public delegate void OnLogFunc(LogLevel type, string msg);
+
         public static event OnLogFunc OnLog;
 
         private static StringBuilder stringBuilder = new StringBuilder(1024);
-        
+
         public static void Info(string msg)
         {
             Log(LogLevel.Info, msg);
         }
-        
+
         public static void Assert(bool condition, string msg = "")
         {
             if (!condition)
             {
-                if (string.IsNullOrEmpty(msg))
-                {
-                    msg = string.Format("{0}", "Assert Failed");
-                }
-                Log(LogLevel.Assert, msg);
+                Log(LogLevel.Assert, string.IsNullOrEmpty(msg) ? "Assert Failed" : msg);
             }
         }
 
@@ -58,7 +55,7 @@ namespace ProxFramework
         {
             Log(LogLevel.Error, msg);
         }
-        
+
         public static void Exception(string msg)
         {
             Log(LogLevel.Exception, msg);
@@ -66,116 +63,91 @@ namespace ProxFramework
 
         private static void Log(LogLevel type, string logString)
         {
-            if (outputType == OutputType.None)
-            {
-                return;
-            }
-
-            if (type < filterLevel)
+            if (outputType == OutputType.None || type < filterLevel)
             {
                 return;
             }
 
             StringBuilder infoBuilder = GetFormatString(type, logString);
 
-            // 获取C#堆栈,Warning以上级别日志才获取堆栈
-             if (type == LogLevel.Error || type == LogLevel.Warning || type == LogLevel.Exception)
-             {
-                 infoBuilder.AppendFormat("\n");
-                 StackFrame[] stackFrames = new StackTrace().GetFrames();
-                 for (int i = 0; i < stackFrames.Length; i++)
-                 {
-                     StackFrame frame = stackFrames[i];
-                     string declaringTypeName = frame.GetMethod().DeclaringType.FullName;
-                     string methodName = stackFrames[i].GetMethod().Name;
-            
-                     infoBuilder.AppendFormat("{0}::{1}\n", declaringTypeName, methodName);
-                 }
-             }
+            if (type >= LogLevel.Warning)
+            {
+                AppendStackTrace(infoBuilder);
+            }
 
             string logStr = infoBuilder.ToString();
-            
-            if (type == LogLevel.Info || type == LogLevel.Success)
+
+            switch (type)
             {
-                UnityEngine.Debug.Log(logStr);
-            }
-            else if (type == LogLevel.Warning)
-            {
-                UnityEngine.Debug.LogWarning(logStr);
-            }
-            else if (type == LogLevel.Assert)
-            {
-                UnityEngine.Debug.LogAssertion(logStr);
-            }
-            else if (type == LogLevel.Error)
-            {
-                UnityEngine.Debug.LogError(logStr);
-            }
-            else if (type == LogLevel.Exception)
-            {
-                UnityEngine.Debug.LogError(logStr);
+                case LogLevel.Info:
+                case LogLevel.Success:
+                    UnityEngine.Debug.Log(logStr);
+                    break;
+                case LogLevel.Warning:
+                    UnityEngine.Debug.LogWarning(logStr);
+                    break;
+                case LogLevel.Assert:
+                    UnityEngine.Debug.LogAssertion(logStr);
+                    break;
+                case LogLevel.Error:
+                case LogLevel.Exception:
+                    UnityEngine.Debug.LogError(logStr);
+                    break;
             }
 
-            if (OnLog != null)
-            {
-                OnLog.Invoke(type, logStr);
-            }
+            OnLog?.Invoke(type, logStr);
         }
 
         private static StringBuilder GetFormatString(LogLevel logLevel, string logString)
         {
             stringBuilder.Clear();
+            string timestamp = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff");
+            logString = logString.Replace("<", "[").Replace(">", "]").Replace("\0", "");
+
             switch (logLevel)
             {
                 case LogLevel.Success:
-                {
                     stringBuilder.AppendFormat(
-                        "><color=gray><b>[INFO] ► </b></color>[{0}] - <color=#00FF18>{1}</color>",
-                        System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff"), logString);
-                }
+                        "><color=gray><b>[INFO] ► </b></color>[{0}] - <color=#00FF18>{1}</color>", timestamp,
+                        logString);
                     break;
                 case LogLevel.Info:
-                {
-                    stringBuilder.AppendFormat(
-                        "<color=gray><b>[INFO] ► </b></color>[{0}] - <color=gray>{1}</color>",
-                        System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff"), logString);
-                }
+                    stringBuilder.AppendFormat("<color=gray><b>[INFO] ► </b></color>[{0}] - <color=gray>{1}</color>",
+                        timestamp, logString);
                     break;
                 case LogLevel.Assert:
-                {
                     stringBuilder.AppendFormat(
-                        "<color=#FF00BD><b>[ASSERT] ► </b></color>[{0}] - <color=green>{1}</color>",
-                        System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff"), logString);
-                }
+                        "<color=#FF00BD><b>[ASSERT] ► </b></color>[{0}] - <color=green>{1}</color>", timestamp,
+                        logString);
                     break;
                 case LogLevel.Warning:
-                {
                     stringBuilder.AppendFormat(
-                        "<color=#FF9400><b>[WARNING] ► </b></color>[{0}] - <color=yellow>{1}</color>",
-                        System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff"), logString);
-                }
+                        "<color=#FF9400><b>[WARNING] ► </b></color>[{0}] - <color=yellow>{1}</color>", timestamp,
+                        logString);
                     break;
                 case LogLevel.Error:
-                {
-                    logString = logString.Replace("<", "[");
-                    logString = logString.Replace(">", "]");
-                    stringBuilder.AppendFormat(
-                        "<color=red><b>[ERROR] ► </b></color>[{0}] - <color=red>{1}</color>",
-                        System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff"), logString.Replace("\0", ""));
-                }
+                    stringBuilder.AppendFormat("<color=red><b>[ERROR] ► </b></color>[{0}] - <color=red>{1}</color>",
+                        timestamp, logString);
                     break;
                 case LogLevel.Exception:
-                {
-                    logString = logString.Replace("<", "[");
-                    logString = logString.Replace(">", "]");
-                    stringBuilder.AppendFormat(
-                        "<color=red><b>[EXCEPTION] ► </b></color>[{0}] - <color=red>{1}</color>",
-                        System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff"), logString.Replace("\0", ""));
-                }
+                    stringBuilder.AppendFormat("<color=red><b>[EXCEPTION] ► </b></color>[{0}] - <color=red>{1}</color>",
+                        timestamp, logString);
                     break;
             }
 
             return stringBuilder;
+        }
+
+        private static void AppendStackTrace(StringBuilder infoBuilder)
+        {
+            infoBuilder.AppendLine();
+            StackFrame[] stackFrames = new StackTrace().GetFrames();
+            foreach (var frame in stackFrames)
+            {
+                string declaringTypeName = frame.GetMethod().DeclaringType.FullName;
+                string methodName = frame.GetMethod().Name;
+                infoBuilder.AppendLine($"{declaringTypeName}::{methodName}");
+            }
         }
     }
 }
