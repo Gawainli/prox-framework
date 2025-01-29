05e77be6f328373c6d621df948c0d149c6b247a3
Gawain <gawainli@outlook.com>
2025-01-30 02:10:33 +0800

    feat:new gen ui code

---
+++ Assets/_Game/Code/HotFix/UI/UITestAssetsWindow.Gen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UI;
+using ProxFramework.UI;
+using TMPro;
+
+namespace Prox.GameName.UI
+{
+    public partial class UITestAssetsWindow : UIWindow
+    {
+        //==自动化变量开始
+        public RawImage rawTest; 
+        public Button btnLoadTex; 
+        public Button btnReleaseTex; 
+
+        private void AutoBind()
+        {
+            //==自动化路径开始
+            rawTest = Q<RawImage>("Raw_Test"); 
+            btnLoadTex = Q<Button>("Btn_LoadTex"); 
+            btnReleaseTex = Q<Button>("Btn_ReleaseTex"); 
+
+        }
+    }
+}
---
+++ Assets/_Game/Code/HotFix/UI/UITestAssetsWindow.cs
@@ -0,0 +1,49 @@
+﻿using Cysharp.Threading.Tasks;
+using ProxFramework.Asset;
+using ProxFramework.UI;
+using UnityEngine;
+
+namespace Prox.GameName.UI
+{
+    [UIWindowAttribute(0, false)]
+    public partial class UITestAssetsWindow : UIWindow
+    {
+        public override void OnCreate()
+        {
+            AutoBind();
+        }
+
+        public override void OnRefresh()
+        {
+            btnLoadTex.onClick.AddListener(LoadTexture);
+            btnReleaseTex.onClick.AddListener(ReleaseTexture);
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+        }
+
+        public override void OnDestroy()
+        {
+            btnLoadTex.onClick.RemoveListener(LoadTexture);
+            btnReleaseTex.onClick.RemoveListener(ReleaseTexture);
+        }
+        
+        private void LoadTexture()
+        {
+            AssetModule.LoadAssetAsync<Texture2D>("Assets/_Game/Art/UI/test.jpg").ContinueWith(tex =>
+            {
+                rawTest.texture = tex;
+            }).Forget();
+        }
+        
+        private void ReleaseTexture()
+        {
+            var tex = rawTest.texture;
+            rawTest.texture = null;
+            // AssetModule.ReleaseAsset("Assets/_Game/Art/UI/test.jpg");
+            AssetModule.ReleaseAsset(tex);
+            AssetModule.UnloadUnusedAssets().Forget();
+        }
+    }
+}
