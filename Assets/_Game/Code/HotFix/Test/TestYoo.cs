2cfe4d2f57d7c8d49a918acfd9d508d4e2d2bb96
Gawain <gawainli@outlook.com>
2025-01-30 02:11:25 +0800

    feat:new test asset ui

--- Assets/_Game/Code/HotFix/Test/TestYoo.cs
+++ Assets/_Game/Code/HotFix/Test/TestYoo.cs
@@ -1,47 +1,25 @@
 using Cysharp.Threading.Tasks;
-using ProxFramework.Asset;
+using Prox.GameName.UI;
+using ProxFramework.UI;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Prox.GameName.HotFix
 {
     public class TestYoo : MonoBehaviour
     {
-        public RawImage rawImage;
-        public Button button;
-
         private void Awake()
         {
-            button.onClick.AddListener(ReleaseTexture);
+            UIModule.OpenWindowAsync<UITestAssetsWindow>("Assets/_Game/Art/UI/Prefabs/UITestAssetsWindow.prefab").Forget();
         }
 
         // Start is called before the first frame update
-        private async void Start()
+        private void Start()
         {
-            var tex = await AssetModule.LoadAssetAsync<Texture2D>("Assets/AssetsArt/UI/test.jpg");
-
-            Debug.Log(tex.GetHashCode());
-            await AssetModule.UnloadUnusedAssets();
-            // var tex2 = await AssetModule.LoadAssetAsync<Texture2D>("Assets/AssetsArt/UI/test.jpg");
-            var tex2 = UnityEngine.Object.Instantiate(tex);
-            Debug.Log(tex2.GetHashCode());
-
-            rawImage.texture = tex2;
-            rawImage.SetNativeSize();
         }
 
         // Update is called once per frame
         void Update()
         {
-        }
-
-        private void ReleaseTexture()
-        {
-            var tex = rawImage.texture;
-            rawImage.texture = null;
-            AssetModule.ReleaseAsset("Assets/AssetsArt/UI/test.jpg");
-            // AssetModule.ReleaseAsset(tex);
-            AssetModule.UnloadUnusedAssets().Forget();
         }
     }
 }
