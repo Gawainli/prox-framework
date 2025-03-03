dba0b2953ba0f61dadbe58fb2504c9d168261a8c
LiJun <gawainli@outlook.com>
2025-03-04 00:09:46 +0800

    refactor:rename gamemain to GameEntrance

---
+++ Assets/_Game/Code/HotUpdate/GameEntrance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using cfg;
+using Cysharp.Threading.Tasks;
+using GameName.Base.PlaySys;
+using GameName.DataTable;
+using GameName.GamePlay;
+using ProxFramework;
+using ProxFramework.Localization;
+
+namespace GameName.Core
+{
+    public static class GameEntrance
+    {
+        public static async void Entrance()
+        {
+            try
+            {
+                await InitSys();
+            }
+            catch (Exception e)
+            {
+                PLogger.Error(e.ToString());
+            }
+        }
+
+        public static async UniTask InitSys()
+        {
+            //Initialize i18n
+            await LocalizationModule.Initialize(new I18NTable());
+
+            //Initialize GameSysCore
+            GameSysCore.Initialize();
+            GameSysCore.RegisterSystem(new DataTableSystem());
+            await GameSysCore.GetSystem<DataTableSystem>().LoadAllTables();
+            GameSysCore.RegisterSystem(new GamePlaySys());
+        }
+    }
+}
